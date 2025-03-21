77ad60a2ef59d7c97ae931c267a1ae9fce5a95ce
Coders Club <nurkarsatar@gmail.com>
2025-03-21 23:35:41 +0300

    space deleted

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import MyProjects from "@/components/MyProjects";
 import Technologies from "@/components/Technologies";
 
 export default function Home() {
+  
   return (
     <div>
       <FirstIntro />
